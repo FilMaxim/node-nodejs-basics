9880ab970fcc1bb9e07ea70efe8d8138c70dd22b
Maxim <fil.maxim13@gmail.com>
2024-03-15 23:04:04 +0300

    feat: implement a function create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'url';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { dirname, join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fresh.txt');
+    const fileContent = 'I am fresh and young';
+
+    // Проверяем существование папки
+    if (!existsSync(folderPath)) {
+        mkdirSync(folderPath);
+    }
+
+    // Проверяем существование файла
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed: File already exists');
+    }
+
+    // Создаем новый файл и записываем в него содержимое
+    writeFileSync(filePath, fileContent);
+
+    console.log('fresh.txt успешно создан с содержимым "I am fresh and young"');
 };
 
 await create();
